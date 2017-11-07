f79bf1dbce07e430ceb76c47c6bc447aa90744f4
sung-su.kim <sung-su.kim@samsung.com>
2017-11-07 16:09:31 +0900

    Add Tizen project for Phoneword

---
+++ Phoneword/Phoneword/Phoneword.Tizen/PhoneDialer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Tizen.Applications;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Phoneword.PhoneDialer))]
+
+namespace Phoneword
+{
+    class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            var request = new Tizen.Applications.AppControl()
+            {
+                Operation = Tizen.Applications.AppControlOperations.Call,
+                Uri = "tel:" + number
+            };
+            try
+            {
+                Tizen.Log.Info("Phoneword", "Calling " + request.Uri);
+                Tizen.Applications.AppControl.SendLaunchRequest(request, OnLaunchResult);
+                return true;
+            }
+            catch (Exception exc)
+            {
+                Tizen.Log.Error("Phoneword", "Failed to dial: " + exc.Message);
+                return false;
+            }
+        }
+
+        private void OnLaunchResult(AppControl launchRequest, AppControl replyRequest, AppControlReplyResult result)
+        {
+            Tizen.Log.Info("Phoneword", $"Result of the request: {result}");
+        }
+    }
+}
