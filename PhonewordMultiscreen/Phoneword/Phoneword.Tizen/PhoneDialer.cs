683f1f1c61f2d5daf6ab2292c8c14894382fbae3
sung-su.kim <sung-su.kim@samsung.com>
2017-11-08 10:57:50 +0900

    Add Tizen project for PhonewordMultiscreen

---
+++ PhonewordMultiscreen/Phoneword/Phoneword.Tizen/PhoneDialer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Tizen.Applications;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Phoneword.PhoneDialer))]
+
+namespace Phoneword
+{
+    class PhoneDialer : IDialer
+    {
+        protected const string Tag = "PhoneWordMultiscreen";
+
+        public bool Dial(string number)
+        {
+            var request = new Tizen.Applications.AppControl()
+            {
+                Operation = Tizen.Applications.AppControlOperations.Call,
+                Uri = "tel:" + number
+            };
+            try
+            {
+                Tizen.Log.Info(Tag, "Calling " + request.Uri);
+                Tizen.Applications.AppControl.SendLaunchRequest(request, OnLaunchResult);
+                return true;
+            }
+            catch (Exception exc)
+            {
+                Tizen.Log.Error(Tag, "Dialing failed: " + exc.Message);
+                return false;
+            }
+        }
+
+        private void OnLaunchResult(AppControl launchRequest, AppControl replyRequest, AppControlReplyResult result)
+        {
+            Tizen.Log.Info(Tag, $"Result of the request: {result}");
+        }
+    }
+}
