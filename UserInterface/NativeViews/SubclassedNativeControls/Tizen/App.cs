49631a100f7dca11367007e4d42f6000612cc5e1
Joonghyun Cho <jh5.cho@samsung.com>
2017-11-22 17:02:46 +0900

    Add Tizen project for NativeViews/SubclassedNativeControls

---
+++ UserInterface/NativeViews/SubclassedNativeControls/Tizen/App.cs
@@ -0,0 +1,23 @@
+﻿using ElmSharp;
+using Xamarin.Forms.Platform.Tizen;
+
+namespace SubclassedNativeControls.Tizen
+{
+    class Program : FormsApplication
+    {
+        public static Window ElmWindow;
+        protected override void OnCreate()
+        {
+            ElmWindow = MainWindow;
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
---
+++ UserInterface/NativeViews/SubclassedNativeControls/Tizen/MyHoversel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Tizen;
+
+namespace SubclassedNativeControls.Tizen
+{
+    class MyHoversel : ElmSharp.Hoversel
+    {
+        IList<string> items;
+        string selectedItem;
+
+        public IList<string> ItemsSource
+        {
+            get { return items; }
+            set
+            {
+                if (items != value)
+                {
+                    items = value;
+                    foreach (string item in items)
+                    {
+                        this.AddItem(item);
+                    }
+                }
+            }
+        }
+
+        public string SelectedItem
+        {
+            get { return selectedItem; }
+            internal set
+            {
+                if (selectedItem != value)
+                {
+                    selectedItem = value;
+                    UpdateSelectedItem();
+                }
+            }
+        }
+
+        public MyHoversel () : base(Program.ElmWindow)
+        {
+            HoverParent = Program.ElmWindow;
+            if (Device.Idiom == TargetIdiom.Phone)
+                Color = ElmSharp.Color.Black;
+            IsHorizontal = false;
+
+            UpdateSelectedItem();
+            
+            ItemSelected += (s, e) =>
+            {
+                SelectedItem = e.Item.Label;
+            };
+        }
+
+        void UpdateSelectedItem()
+        {
+            Text = SelectedItem;
+        }
+    }
+}
