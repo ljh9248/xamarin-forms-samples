5408e575705f0669f2346bbd8e06826180d6138f
Joonghyun Cho <jh5.cho@samsung.com>
2017-11-23 15:08:10 +0900

    Add Tizen project for NativeViews/NativeViewInsideContentView

--- UserInterface/NativeViews/NativeViewInsideContentView/NativeViewInsideContentView/NativeViewInsideContentViewPage.xaml.cs
+++ UserInterface/NativeViews/NativeViewInsideContentView/NativeViewInsideContentView/NativeViewInsideContentViewPage.xaml.cs
@@ -30,6 +30,17 @@ namespace NativeViewInsideContentView
 			var button = (Windows.UI.Xaml.Controls.Button)buttonWrapper.NativeElement;
             button.Click += (sender, args) => OnButtonTap(sender, EventArgs.Empty);
 #endif
+#if __Tizen__
+			contentViewTextParent.HorizontalOptions = LayoutOptions.FillAndExpand;
+			contentViewTextParent.VerticalOptions = LayoutOptions.FillAndExpand;
+			var labelWrapper = (Xamarin.Forms.Platform.Tizen.EvasObjectWrapper)contentViewTextParent.Content;
+			var label = (ElmSharp.Label)labelWrapper.EvasObject;
+			label.TextStyle = "DEFAULT='color=#ff0000 font_size=30 align=center'";
+			label.SetVerticalTextAlignment("elm.text", 0.5);
+
+			var buttonWrapper = (Xamarin.Forms.Platform.Tizen.EvasObjectWrapper)contentViewButtonParent.Content;
+			buttonWrapper.WidthRequest = 500;
+#endif
 		}
 
 		async void OnButtonTap(object sender, EventArgs e)
---
+++ UserInterface/NativeViews/NativeViewInsideContentView/Tizen/App.cs
@@ -0,0 +1,25 @@
+﻿using ElmSharp;
+using Xamarin.Forms.Platform.Tizen;
+
+namespace NativeViewInsideContentView.Tizen
+{
+    class Program : FormsApplication
+    {
+        public static Window ElmWindow;
+
+        protected override void OnCreate()
+        {
+            ElmWindow = MainWindow;
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            Forms.Init(app);
+            app.Run(args);
+        }
+    }
+
+}
