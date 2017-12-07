99c85a556b537ab3af2e58530aa375e8a1ed0459
darkleem <cdark.lim@samsung.com>
2017-12-07 13:58:29 +0900

    Add FormsGallery project

---
+++ FormsGallery/FormsGallery.Tizen/App.cs
@@ -0,0 +1,20 @@
+﻿
+namespace FormsGallery.Tizen
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.FormsMaps.Init("HERE", "Enter user Key please");
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
